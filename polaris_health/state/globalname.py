b1d0ee711f495893466d335513ec0893abd93e5b
Anton Gavrik <agavrik@verisign.com>
2015-08-31 13:46:35 +0000

    cleanup pass

--- polaris_health/state/globalname.py
+++ polaris_health/state/globalname.py
@@ -23,35 +23,33 @@ class GlobalName:
             name: str, DNS name to be load-balanced
             pool_name: str, name of a pool to load balance against
             ttl: int, ttl value to return with responses
+        
         """
         # name
+        self.name = name.lower() # lowcase fqdns
         if (not isinstance(name, str) or len(name) == 0 
                 or len(name) > MAX_NAME_LEN):
             log_msg = ('name "{}" must be a non-empty str, {} chars max'
                        .format(name, MAX_NAME_LEN))
             LOG.error(log_msg)
             raise Error(log_msg)
-        else:
-            self.name = name
 
         # pool_name
+        self.pool_name = pool_name
         if (not isinstance(pool_name, str) or len(pool_name) == 0
                 or len(pool_name) > MAX_POOL_NAME_LEN):
             log_msg = ('pool_name "{}" must be a non-empty str, {} chars max'
                        .format(pool_name, MAX_POOL_NAME_LEN))
             LOG.error(log_msg)
             raise Error(log_msg)
-        else:
-            self.pool_name = pool_name
 
         # ttl
+        self.ttl = ttl
         if (not isinstance(ttl, int) or ttl < MIN_TTL):
             log_msg = ('ttl must be an int greater or equal {}'
                        .format(ttl, MIN_TTL))
             LOG.error(log_msg)
             raise Error(log_msg)
-        else:
-            self.ttl = ttl
 
     @classmethod
     def from_config_dict(cls, name, obj):
@@ -60,6 +58,7 @@ class GlobalName:
         args:
             name: str, name of the globalname
             obj: dict, config dict
+
         """
         if 'pool' not in obj:
             log_msg = ('"{}" is missing a mandatory parameter "pool"'
@@ -76,14 +75,15 @@ class GlobalName:
         return cls(name=name, pool_name=obj['pool'], ttl=obj['ttl'])
 
     def to_dist_dict(self):
-        """Return a dict representation of the GlobalName required to perform
-        distribution.
+        """Return a dict representation of the GlobalName required by 
+        Polaris PDNS to perform distribution.
 
         Example:
             {
                 'pool_name': 'pool1',
                 'ttl': 1
             }
+
         """
         obj = {}
         obj['pool_name'] = self.pool_name
--- polaris_health/state/pool.py
+++ polaris_health/state/pool.py
@@ -20,7 +20,7 @@ MAX_REGION_LEN = 256
 MAX_MAX_ADDRS_RETURNED = 32
 
 def pprint_status(status):
-    """convert a bool into a server status string"""
+    """Convert bool status into a health status string"""
 
     if status is True:
         return 'UP'
@@ -44,6 +44,7 @@ class PoolMember:
                 is disabled
             region: string, id of the region, used in topology-based 
                 distribution
+        
         """
         ### ip
         try:
@@ -55,7 +56,7 @@ class PoolMember:
             raise Error(log_msg)
 
         if _ip.version != 4:
-            log_msg = 'only v4 IP addresses are supported'
+            log_msg = 'only v4 IP addresses are currently supported'
             LOG.error(log_msg)
             raise Error(log_msg)
 
@@ -130,16 +131,16 @@ class Pool:
                 "nodata": return an empty RR-set
             max_addrs_returned: int, max number of A records to return in
                 response
+        
         """
         ### name
+        self.name = name
         if (not isinstance(name, str) 
                 or len(name) > MAX_POOL_NAME_LEN):
             log_msg = ('"{}" name must be a str, {} chars max'.
                        format(name, MAX_POOL_NAME_LEN))
             LOG.error(log_msg)
             raise Error(log_msg)
-        else:
-            self.name = name
 
         ### monitor
         self.monitor = monitor
@@ -148,6 +149,7 @@ class Pool:
         self.members = members
 
         ### lb_method
+        self.lb_method = lb_method
         if (not isinstance(lb_method, str) 
                 or lb_method not in self.LB_METHOD_OPTIONS):
             _lb_methods = ', '.join(self.LB_METHOD_OPTIONS)
@@ -155,10 +157,9 @@ class Pool:
                        format(lb_method, _lb_methods)) 
             LOG.error(log_msg)         
             raise Error(log_msg)
-        else:
-            self.lb_method = lb_method
 
         ### fallback
+        self.fallback = fallback
         if (not isinstance(fallback, str) 
                 or fallback not in self.FALLBACK_OPTIONS):
             _fallbacks = ', '.join(self.FALLBACK_OPTIONS)
@@ -166,10 +167,9 @@ class Pool:
                        format(fallback, _fallbacks))
             LOG.error(log_msg)         
             raise Error(log_msg)
-        else:
-            self.fallback = fallback      
 
-        # max_addrs_returned    
+        # max_addrs_returned
+        self.max_addrs_returned = max_addrs_returned
         if (not isinstance(max_addrs_returned, int) or max_addrs_returned < 1 
                 or max_addrs_returned > MAX_MAX_ADDRS_RETURNED):
             log_msg = ('"{}" max_addrs_returned "{}" must be an int '
@@ -177,8 +177,6 @@ class Pool:
                        .format(name, max_addrs_returned, 
                                MAX_MAX_ADDRS_RETURNED))
             raise Error(log_msg)
-        else:
-            self.max_addrs_returned = max_addrs_returned
 
         # last known status None, True, False
         self.last_status = None
@@ -290,18 +288,19 @@ class Pool:
                    **pool_optional_params)
 
     def to_dist_dict(self):
-        """Return dict representation of the Pool required to perform 
-        distribution.
+        """Return dict representation of the Pool required  by Polaris PDNS
+        to perform query distribution.
 
         "_default" distribution table is always built.
 
-        Region-specific distribution tables are only built if the pool is using 
+        Region-specific distribution tables are built only if the pool is using 
         a topology lb method and has an UP member.
 
         Example:
             {
                 'status' : True,
                 'lb_method': 'twrr',
+                'fallback': 'any', 
                 'max_addrs_returned': 1,
                 'dist_tables': {
                     '_default': {
@@ -333,6 +332,9 @@ class Pool:
         ### lb_method  
         obj['lb_method'] = self.lb_method
 
+        ### fallback
+        obj['fallback'] = self.fallback
+
         ### max_addrs_returned
         obj['max_addrs_returned'] = self.max_addrs_returned
 
@@ -343,45 +345,86 @@ class Pool:
         dist_tables['_default'] = {}
         dist_tables['_default']['rotation'] = []
         dist_tables['_default']['num_unique_addrs'] = 0
-        
-        for member_ip in self.members:
-            member = self.members[member_ip]
+       
+        ##################
+        ### pool is UP ###
+        ##################
+        if self.status:
+            for member_ip in self.members:
+                member = self.members[member_ip]
 
-            # ignore members with weight of 0 - member is disabled
-            if member.weight == 0:
-                continue
+                # do not add members with the weight of 0 - member is disabled
+                if member.weight == 0:
+                    continue
 
-            # add the member IP times it's weight into 
-            # the_default distribution table
-            for i in range(member.weight):
-                dist_tables['_default']['rotation'].append(member_ip)
-                # increase the number of unique addresses
+                # do not add members in DOWN state
+                if not member.status:
+                    continue
 
-            # increase the number of unique addresses in the _default by __1__
-            dist_tables['_default']['num_unique_addrs'] += 1
+                #
+                # add to the _default table
+                #
 
-            # if a topology lb method is used and the member is UP 
-            # add it to a corresponding regiinal table 
-            if self.lb_method == 'twrr' and member.status:
-                # create the regional table if it does not exist
-                if member.region not in dist_tables:
-                    dist_tables[member.region] = {}
-                    dist_tables[member.region]['rotation'] = []
-                    dist_tables[member.region]['num_unique_addrs'] = 0
-
-                # add the member IP it's weight into the regional table
+                # add the member IP times it's weight into 
+                # the _default distribution table
                 for i in range(member.weight):
-                    dist_tables[member.region]['rotation'].append(member_ip)
+                    dist_tables['_default']['rotation'].append(member_ip)
 
-                # increase the number of unique addresses in the table by __1__
-                dist_tables[member.region]['num_unique_addrs'] += 1
+                # increase the number of unique addresses in the _default by 1
+                dist_tables['_default']['num_unique_addrs'] += 1
+        
+                #
+                # add to a regional table
+                #
 
+                # if a topology lb method is used, add the member's IP 
+                # to a corresponding regional table 
+                if self.lb_method == 'twrr':
+                    # create the regional table if it does not exist
+                    if member.region not in dist_tables:
+                        dist_tables[member.region] = {}
+                        dist_tables[member.region]['rotation'] = []
+                        dist_tables[member.region]['num_unique_addrs'] = 0
+
+                    # add the member IP times it's weight
+                    # into the regional table
+                    for i in range(member.weight):
+                        dist_tables[member.region]['rotation'].append(member_ip)
+
+                    # increase the number of unique addresses in the table by 1
+                    dist_tables[member.region]['num_unique_addrs'] += 1
+
+        ####################
+        ### pool is DOWN ###
+        ####################
+        else:
+            for member_ip in self.members:
+                member = self.members[member_ip]
+
+                # do not add members with weight of 0 - member is disabled
+                if member.weight == 0:
+                    continue
+
+                # add to the _default table if fallback is set to 'any'
+                if self.fallback == 'any':
+                    # add the member IP times it's weight into
+                    # the _default distribution table
+                    for i in range(member.weight):
+                        dist_tables['_default']['rotation'].append(member_ip)
+
+                    # increase the number of unique addresses
+                    # in the _default by 1
+                    dist_tables['_default']['num_unique_addrs'] += 1
+
+        ##################################
+        ### post tables creation tasks ###
+        ##################################
         for name in dist_tables:
             # randomly shuffle the rotation list
             random.shuffle(dist_tables[name]['rotation']) 
        
-            # create index used by distributor for distribution,
-            # set it to a random position, when distributor is
+            # create index used by ppdns for distribution,
+            # set it to a random position, when ppdns is
             # syncing its internal state from shared memory, indexes gets
             # reset, we want to avoid starting from 0 every time
             dist_tables[name]['index'] = \
