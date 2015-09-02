dcef758f7f5cef42ea0d4f195148d616ff20f71e
Anton Gavrik <antongavrik@gmail.com>
2015-09-02 13:15:01 +0200

    Update get-generic-state.py

--- admin-tools/get-generic-state.py
+++ admin-tools/get-generic-state.py
@@ -10,5 +10,5 @@ mc = memcache.Client(['127.0.0.1'])
 
 val = mc.get('polaris_health:generic_state')
 print(json.dumps(val, indent=4))
-#print('\nTimestamp:', time.asctime(time.gmtime(val['timestamp'])))
+print('\nTimestamp:', time.asctime(time.gmtime(val['timestamp'])))
 
